3f6d2b6e31fe8d044a67f3e50e39b220e3137d41
Collins Mony <collins.ferdinand@gmail.com>
2016-04-22 12:44:14 -0500

    GTM and GA Installation

--- wp-content/themes/magzilla/header.php
+++ wp-content/themes/magzilla/header.php
@@ -55,6 +55,20 @@ global $ft_option, $fave_container; // Fetch options stored in $ft_option;
 	<?php } ?>
 
 	<?php wp_head(); ?>
+
+<!-- Google Analytics -->
+<script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-2684182-29', 'auto');
+  ga('send', 'pageview');
+
+</script>
+<!-- End Google Analytics -->
+
 </head>
 
 <?php 
@@ -80,6 +94,17 @@ if ( $main_layout == "container-fluid" ) {
 ?>
 
 <body <?php body_class( $boxed_layout ); ?>>
+
+<!-- Google Tag Manager -->
+<noscript><iframe src="//www.googletagmanager.com/ns.html?id=GTM-556JXJ"
+height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'//www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-556JXJ');</script>
+<!-- End Google Tag Manager -->
+
 
 	<div id="fb-root"></div>
 	<script>(function(d, s, id) {
